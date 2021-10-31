3d77c4a2a4bdced46f4a1550c0f05a84eb08fed5
Rachel Cameron <rachleecameron@gmail.com>
2021-10-31 19:56:53 -0400

    App Class

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -1,5 +1,10 @@
 package ucf.assignments;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
 public class App {
     public static void main(String[] args) {
 
